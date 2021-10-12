a5f1a338e5ea4d60bc10194266922273afdfd010
shumit <shumitt@gmail.com>
2021-10-12 22:42:10 +0600

    ok

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Services from './../Services/Services';
+import Experts from './../Experts/Experts';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner/>
+            <Services/>
+            <Experts/>
+            
+        </div>
+    )
+}
+
+export default Home
